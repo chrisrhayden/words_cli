0e4063ffe15db1d08898201652d41ac03e2600d5
chrisrhayden <chrisrhayden@gmail.com>
2020-09-14 13:15:43 -0700

    add suggest and define clia args and small clean ups

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 mod cache;
 mod dict_api;
 mod formatter;
+
+#[cfg(test)]
 mod test_utils;
-mod utils;
 
 use std::error::Error;
 
 use clap::Clap;
 
-use cache::{cache_word, get_from_cache};
+use cache::{cache_definition, get_from_cache};
 use dict_api::{get_definition, RequestOptions};
 use formatter::print_definition;
 
 #[allow(dead_code)]
 #[derive(Clap)]
-#[clap()]
+#[clap(name = "words_cli", about = "a tool for words")]
 struct WordArgs {
     #[clap(short, long)]
     columns: Option<usize>,
+    #[clap(short, long, conflicts_with = "define")]
+    suggest: bool,
+    #[clap(short, long, conflicts_with = "suggest")]
+    define: bool,
     #[clap(required = true)]
     query: String,
 }
@@ -25,18 +30,26 @@ struct WordArgs {
 fn main() -> Result<(), Box<dyn Error>> {
     let args = WordArgs::parse();
 
-    let cached_query = get_from_cache(&args.query)?;
-
-    if let Some(cached_query) = cached_query {
-        print_definition(&cached_query);
+    if args.suggest {
+        // TODO:
+        println!("suggestions aren't implemented yet");
     } else {
-        let request_opts = RequestOptions::default();
+        let cached_query = get_from_cache(&args.query)?;
 
-        let word_data = get_definition(request_opts, &args.query)?;
+        let word_data = if let Some(cached_query) = cached_query {
+            cached_query
+        } else {
+            let request_opts = RequestOptions::default();
 
-        cache_word(&word_data)?;
+            let word_data = get_definition(request_opts, &args.query)?;
+
+            cache_definition(&word_data)?;
+
+            word_data
+        };
 
         print_definition(&word_data);
     }
+
     Ok(())
 }
