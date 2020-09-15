aace2431de85a20c05496549b54a112056753251
chrisrhayden <chrisrhayden@gmail.com>
2020-09-15 01:36:56 -0700

    add a way to use stdin; add comments

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod utils;
 #[cfg(test)]
 mod test_utils;
 
-use std::error::Error;
+use std::{error::Error, io};
 
 use clap::Clap;
 
@@ -19,9 +19,11 @@ use crate::{
     utils::get_tty_cols,
 };
 
-#[allow(dead_code)]
 #[derive(Clap)]
-#[clap(name = "words_cli", about = "a tool for words")]
+#[clap(name = "words_cli")]
+/// a tool for words
+///
+/// NOTE: you can specify stdin by giving a -
 struct WordArgs {
     /// print word suggestions
     #[clap(short, long, conflicts_with = "define")]
@@ -30,24 +32,59 @@ struct WordArgs {
     #[clap(short, long, conflicts_with = "suggest")]
     define: Option<String>,
     /// columns to align definition text
+    ///
+    /// this will make the definition text stay within the specified columns
     #[clap(short, long)]
     columns: Option<usize>,
+}
+
+// read input from stdin if asked for
+fn get_from_stdin() -> Result<String, Box<dyn Error>> {
+    use std::io::Read;
+
+    // get and lock stdin to read safely from stdin
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+
+    let mut buf = String::new();
+
+    stdin.read_to_string(&mut buf)?;
+
+    if buf.is_empty() {
+        Err(Box::from("nothing in stdin"))
+    } else {
+        let output = buf.trim().to_string();
+
+        Ok(output)
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = WordArgs::parse();
 
-    if let Some(query) = args.suggest {
-        if let Some(suggest_list) = check_spelling(&query)? {
+    if let Some(mut query) = args.suggest {
+        if query == "-" {
+            query = get_from_stdin()?;
+        }
+
+        let query_str = query.trim();
+
+        if let Some(suggest_list) = check_spelling(query_str)? {
             println!("{}", suggest_list.join("\n"));
         }
-    } else if let Some(query) = args.define {
-        let word_data = if let Some(cached_query) = get_from_cache(&query)? {
+    } else if let Some(mut query) = args.define {
+        if query == "-" {
+            query = get_from_stdin()?;
+        }
+
+        let query_str = query.trim();
+
+        let word_data = if let Some(cached_query) = get_from_cache(query_str)? {
             cached_query
         } else {
             let request_opts = RequestOptions::default();
 
-            let word_data = get_definition(request_opts, &query)?;
+            let word_data = get_definition(request_opts, query_str)?;
 
             cache_definition(&word_data)?;
 
