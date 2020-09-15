445d565709b1aae60d5d9721e99b0feac1863e5a
chrisrhayden <chrisrhayden@gmail.com>
2020-09-14 19:06:00 -0700

    move all segments in to there own function

--- src/formatter.rs
+++ src/formatter.rs
@@ -36,110 +36,225 @@ impl Default for FormatConf {
     fn default() -> Self {
         Self {
             columns: 0,
-            indent_by: 2,
+            indent_by: 4,
             format_style: Default::default(),
         }
     }
 }
 
-fn format_definition(format_conf: &FormatConf, word_data: &WordData) -> String {
+fn break_line(line_break: usize, spaces: &str, line: &str) -> Vec<String> {
+    const SEARCH_LIMIT: usize = 10;
+
+    let mut output: Vec<String> = Vec::new();
+
+    let mut end;
+    let mut start = 0;
+
+    let line_len = line.len();
+
+    while start < line_len {
+        end = start + line_break;
+
+        let n_line = if end > line_len {
+            line.get(start..start + (line_len - start))
+                .expect("cant get the rest of the line")
+        } else {
+            let cur_line = line.get(start..end).expect("out of bounds");
+
+            for (i, c) in cur_line.chars().rev().take(SEARCH_LIMIT).enumerate()
+            {
+                if c == ' ' {
+                    end = end - i;
+
+                    break;
+                }
+            }
+
+            line.get(start..end)
+                .expect("out of bounds, something is wrong")
+        };
+
+        let n_line = format!("{}{}", spaces, n_line);
+
+        output.push(n_line);
+
+        start = end;
+    }
+
+    output
+}
+
+fn format_word(format_conf: &FormatConf, line: &str) -> String {
+    format!(
+        "{}{}{}",
+        format_conf.format_style.word, line, format_conf.format_style.reset
+    )
+}
+
+fn format_part_of(
+    format_conf: &FormatConf,
+    spaces: &str,
+    part_of_speech: &str,
+) -> String {
+    format!(
+        "{}{}{}{}",
+        spaces,
+        format_conf.format_style.part_of_speech,
+        part_of_speech,
+        format_conf.format_style.reset
+    )
+}
+
+fn format_definition(
+    config: &FormatConf,
+    spaces: &str,
+    line: &str,
+) -> Vec<String> {
+    let full_len = spaces.len() + line.len();
+
+    if full_len > config.columns {
+        let break_in_to = config.columns - spaces.len();
+
+        break_line(break_in_to, spaces, line)
+    } else {
+        vec![format!(
+            "{}{}{}{}",
+            spaces,
+            config.format_style.definition,
+            line,
+            config.format_style.reset
+        )]
+    }
+}
+
+fn format_example_title(format_conf: &FormatConf, spaces: &str) -> String {
+    format!(
+        "{}{}example{}",
+        spaces,
+        format_conf.format_style.example_title,
+        format_conf.format_style.reset
+    )
+}
+
+fn format_example(
+    config: &FormatConf,
+    spaces: &str,
+    line: &str,
+) -> Vec<String> {
+    let full_len = spaces.len() + line.len();
+
+    if full_len > config.columns {
+        let break_in_to = config.columns - spaces.len();
+
+        break_line(break_in_to, spaces, line)
+    } else {
+        vec![format!(
+            "{}{}{}{}",
+            spaces,
+            config.format_style.example,
+            line,
+            config.format_style.reset
+        )]
+    }
+}
+
+fn format_synonyms_title(format_conf: &FormatConf, spaces: &str) -> String {
+    format!(
+        "{}{}synonyms{}",
+        spaces,
+        format_conf.format_style.synonyms_title,
+        format_conf.format_style.reset
+    )
+}
+
+fn format_synonyms(
+    config: &FormatConf,
+    spaces: &str,
+    line: &str,
+) -> Vec<String> {
+    let full_len = spaces.len() + line.len();
+
+    if full_len > config.columns {
+        let break_in_to = config.columns - spaces.len();
+
+        break_line(break_in_to, spaces, line)
+    } else {
+        vec![format!(
+            "{}{}{}{}",
+            spaces,
+            config.format_style.synonyms,
+            line,
+            config.format_style.reset
+        )]
+    }
+}
+
+fn format_word_data(format_conf: &FormatConf, word_data: &WordData) -> String {
     let mut output: Vec<String> = Vec::new();
 
     let spaces = " ".repeat(format_conf.indent_by);
+    let def_spaces = spaces.repeat(2);
+    let example_spaces = spaces.repeat(3);
 
-    let reset = &format_conf.format_style.reset;
-
-    let word = format!(
-        "{}{}{}",
-        format_conf.format_style.word, word_data.word, reset
-    );
+    let word = format_word(format_conf, &word_data.word);
 
     output.push(word);
 
     let meanings_len = word_data.meanings.len();
 
-    for meaning in &word_data.meanings {
-        let part_of = format!(
-            "{}{}{}{}",
-            spaces,
-            format_conf.format_style.part_of_speech,
-            meaning.partOfSpeech,
-            reset
-        );
+    for (i, meaning) in word_data.meanings.iter().enumerate() {
+        let part_of =
+            format_part_of(format_conf, &spaces, &meaning.partOfSpeech);
 
         output.push(part_of);
 
         let definition_len = meaning.definitions.len();
 
-        for definition in &meaning.definitions {
-            let def_string = format!(
-                "{}{}{}{}{}",
-                spaces,
-                spaces,
-                format_conf.format_style.definition,
-                definition.definition,
-                reset
+        for (i, definition) in meaning.definitions.iter().enumerate() {
+            let definition_lines = format_definition(
+                &format_conf,
+                &def_spaces,
+                &definition.definition,
             );
 
-            output.push(def_string);
+            output.extend(definition_lines);
 
             if let Some(ref example) = definition.example {
                 output.push(String::new());
 
-                let example_title = format!(
-                    "{}{}{}example{}",
-                    spaces,
-                    spaces,
-                    format_conf.format_style.example_title,
-                    reset
-                );
+                let example_title =
+                    format_example_title(format_conf, &def_spaces);
 
                 output.push(example_title);
 
-                let example_str = format!(
-                    "{}{}{}{}{}{}",
-                    spaces,
-                    spaces,
-                    spaces,
-                    format_conf.format_style.example,
-                    example,
-                    reset
-                );
+                let example_lines =
+                    format_example(format_conf, &example_spaces, example);
 
-                output.push(example_str);
+                output.extend(example_lines);
             }
 
             if let Some(ref syns) = definition.synonyms {
                 output.push(String::new());
-                let synonyms_title = format!(
-                    "{}{}{}synonyms{}",
-                    spaces,
-                    spaces,
-                    format_conf.format_style.synonyms_title,
-                    reset
-                );
+
+                let synonyms_title =
+                    format_synonyms_title(format_conf, &def_spaces);
+
                 output.push(synonyms_title);
 
                 for syn in syns {
-                    let syn_str = format!(
-                        "{}{}{}{}{}{}",
-                        spaces,
-                        spaces,
-                        spaces,
-                        format_conf.format_style.synonyms,
-                        syn,
-                        reset
-                    );
-                    output.push(syn_str);
+                    let syn_strs =
+                        format_synonyms(format_conf, &example_spaces, syn);
+
+                    output.extend(syn_strs);
                 }
             }
 
-            if definition_len > 1 {
+            if definition_len > 1 && i + 1 != definition_len {
                 output.push(String::new());
             }
         }
 
-        if meanings_len > 1 {
+        if meanings_len > 1 && i + 1 != meanings_len {
             output.push(String::new());
         }
     }
@@ -148,7 +263,7 @@ fn format_definition(format_conf: &FormatConf, word_data: &WordData) -> String {
 }
 
 pub fn print_definition(format_conf: &FormatConf, word_data: &WordData) {
-    let word_str = format_definition(format_conf, word_data);
+    let word_str = format_word_data(format_conf, word_data);
 
     println!("{}", word_str);
 }
