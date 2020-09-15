7dbfc8d17e224c882f85d0456a072b9e14531f40
chrisrhayden <chrisrhayden@gmail.com>
2020-09-14 21:37:53 -0700

    add ispell and suggestions; add help to args; clean main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod cache;
 mod dict_api;
 mod formatter;
+mod spell;
 mod utils;
 
 #[cfg(test)]
@@ -10,29 +11,38 @@ use std::error::Error;
 
 use clap::Clap;
 
-use cache::{cache_definition, get_from_cache};
-use dict_api::{get_definition, RequestOptions};
-use formatter::{print_definition, FormatConf};
-use utils::get_tty_cols;
+use crate::{
+    cache::{cache_definition, get_from_cache},
+    dict_api::{get_definition, RequestOptions},
+    formatter::{print_definition, FormatConf},
+    spell::check_spelling,
+    utils::get_tty_cols,
+};
 
 #[allow(dead_code)]
 #[derive(Clap)]
 #[clap(name = "words_cli", about = "a tool for words")]
 struct WordArgs {
-    #[clap(short, long)]
-    columns: Option<usize>,
+    /// print word suggestions
     #[clap(short, long, conflicts_with = "define")]
     suggest: Option<String>,
+    /// print word definition
     #[clap(short, long, conflicts_with = "suggest")]
     define: Option<String>,
+    /// columns to align definition text
+    #[clap(short, long)]
+    columns: Option<usize>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = WordArgs::parse();
 
-    if let Some(_query) = args.suggest {
-        // TODO:
-        println!("suggestions aren't implemented yet");
+    if let Some(query) = args.suggest {
+        if let Some(suggest_list) = check_spelling(&query)? {
+            println!("{}", suggest_list.join("\n"));
+        }
+
+        Ok(())
     } else if let Some(query) = args.define {
         let word_data = if let Some(cached_query) = get_from_cache(&query)? {
             cached_query
@@ -55,9 +65,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         };
 
         print_definition(&format_conf, &word_data);
-    } else {
-        eprintln!("nothing to do");
-    }
 
-    Ok(())
+        Ok(())
+    } else {
+        Err(Box::from("we should not be able to get get here"))
+    }
 }
