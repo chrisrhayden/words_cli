c22d420f80cea15a9b9baff0a66445e75fe2de1e
chrisrhayden <chrisrhayden@gmail.com>
2020-09-14 21:37:24 -0700

    updating to use in this project

--- src/spell.rs
+++ src/spell.rs
@@ -1,42 +1,29 @@
-use std::{env, error::Error};
+use std::error::Error;
 
 use ispell::SpellLauncher;
 
-fn parse_args() -> Result<String, Box<dyn Error>> {
-    if env::args().len() == 1 {
-        return Err(Box::from("pleas give a query"));
-    } else if env::args().len() > 2 {
-        return Err(Box::from("too many args"));
-    }
-
-    let query = env::args().nth(1).expect("cant get arg");
-
-    if query.is_empty() {
-        // NOTE: this probably shouldn't happen but idk
-        return Err(Box::from("pleas give query"));
-    }
-
-    Ok(query)
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let query = parse_args()?;
-
+pub fn check_spelling(
+    query: &str,
+) -> Result<Option<Vec<String>>, Box<dyn Error>> {
     let mut checker = SpellLauncher::new().aspell().launch()?;
 
     let spell_errs = checker.check(&query)?;
 
     if spell_errs.is_empty() {
-        println!("{} is spelled correctly", query);
-
-        return Ok(());
+        return Ok(None);
     }
 
+    let mut output = Vec::new();
+
     for se in spell_errs {
+        if se.suggestions.is_empty() {
+            return Err(Box::from(format!("no suggestion for {}", query)));
+        }
+
         for suggestion in se.suggestions {
-            println!("{}", suggestion);
+            output.push(suggestion);
         }
     }
 
-    Ok(())
+    Ok(Some(output))
 }
