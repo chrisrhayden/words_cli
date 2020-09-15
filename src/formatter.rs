3c1f16fb7edf6cd91bc119ec7f30031bbe015c65
chrisrhayden <chrisrhayden@gmail.com>
2020-09-14 23:19:59 -0700

    small fixes; add more test

--- src/formatter.rs
+++ src/formatter.rs
@@ -43,6 +43,7 @@ impl Default for FormatConf {
 }
 
 fn break_line(
+    config: &FormatConf,
     line_break: usize,
     line_style: &str,
     spaces: &str,
@@ -78,7 +79,10 @@ fn break_line(
                 .expect("out of bounds, something is wrong")
         };
 
-        let n_line = format!("{}{}{}", spaces, line_style, n_line);
+        let n_line = format!(
+            "{}{}{}{}",
+            spaces, line_style, n_line, config.format_style.reset
+        );
 
         output.push(n_line);
 
@@ -90,8 +94,16 @@ fn break_line(
 
 macro_rules! format_line {
     ($config:expr, $style:expr, $spaces:expr, $line:expr) => {
-        if ($spaces.len() + $line.len()) > $config.columns {
-            break_line($config.columns - $spaces.len(), $style, $spaces, $line)
+        if $config.columns != 0
+            && ($spaces.len() + $line.len()) > $config.columns
+        {
+            break_line(
+                $config,
+                $config.columns - $spaces.len(),
+                $style,
+                $spaces,
+                $line,
+            )
         } else {
             vec![format!(
                 "{}{}{}{}",
@@ -210,4 +222,147 @@ pub fn print_definition(format_conf: &FormatConf, word_data: &WordData) {
     let word_str = format_word_data(format_conf, word_data);
 
     println!("{}", word_str);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use crate::test_utils::fake_word_data;
+
+    #[test]
+    fn test_format_line_short_line_no_trailing_comma() {
+        let mut conf = FormatConf::default();
+
+        let line_break = 21;
+
+        conf.columns = line_break;
+
+        let style = "";
+        let spaces = "";
+        let line = "this is a short line";
+
+        let lines = format_line!(&conf, style, spaces, line);
+
+        assert_eq!(lines.len(), 1, "broke a short line when it shouldn't");
+
+        assert_eq!(
+            lines.first().unwrap(),
+            "this is a short line\u{1b}[0m",
+            "line mangled"
+        );
+    }
+
+    #[test]
+    fn test_format_line_short_line_trailing_comma() {
+        let mut conf = FormatConf::default();
+
+        let line_break = 20;
+
+        conf.columns = line_break;
+
+        let style = "";
+        let spaces = "";
+        let line = "this is a short line";
+
+        let lines = format_line!(&conf, style, spaces, line,);
+
+        assert_eq!(lines.len(), 1, "broke a short line when it shouldn't");
+
+        assert_eq!(
+            lines.first().unwrap(),
+            "this is a short line\u{1b}[0m",
+            "line mangled"
+        );
+    }
+
+    #[test]
+    fn test_format_line_long_line_with_spaces() {
+        let mut conf = FormatConf::default();
+
+        let line_break = 20;
+
+        conf.columns = line_break;
+
+        let style = "";
+        let spaces = "";
+        let line = "this is a vary long lone that will need a lot of word and \
+                    words and more words to be very long if i really want this \
+                    string to be very long i guess im glad im i dont need it \
+                    to be very unique";
+
+        let lines = format_line!(&conf, style, spaces, line,);
+
+        assert_eq!(
+            lines.len(),
+            11,
+            "broke a long in to wrong amount of segments"
+        );
+
+        let broken_lines: Vec<&'static str> = vec![
+            "this is a vary long \x1b[0m",
+            "lone that will need \x1b[0m",
+            "a lot of word and \x1b[0m",
+            "words and more \x1b[0m",
+            "words to be very \x1b[0m",
+            "long if i really \x1b[0m",
+            "want this string to \x1b[0m",
+            "be very long i \x1b[0m",
+            "guess im glad im i \x1b[0m",
+            "dont need it to be \x1b[0m",
+            "very unique\x1b[0m",
+        ];
+
+        assert_eq!(lines, broken_lines, "broke lines in the wrong way")
+    }
+
+    #[test]
+    fn test_format_line_long_line_without_spaces() {
+        let mut conf = FormatConf::default();
+
+        let line_break = 20;
+
+        conf.columns = line_break;
+
+        let style = "";
+        let spaces = "";
+
+        let line = "11111111111111111111111111111111111111111111111111\
+                    11111111111111111111111111111111111111111111111111";
+
+        let lines = format_line!(&conf, style, spaces, line,);
+
+        assert_eq!(
+            lines.len(),
+            5,
+            "broke a long in to wrong amount of segments"
+        );
+
+        let broken_lines: Vec<&'static str> = vec![
+            "11111111111111111111\x1b[0m",
+            "11111111111111111111\x1b[0m",
+            "11111111111111111111\x1b[0m",
+            "11111111111111111111\x1b[0m",
+            "11111111111111111111\x1b[0m",
+        ];
+
+        assert_eq!(lines, broken_lines, "broke lines in the wrong way")
+    }
+
+    #[test]
+    fn test_format_word_data() {
+        let fake_word = fake_word_data();
+
+        // TODO: this is bad and should change
+        let fake_word_string =
+            "\x1b[1mtest\x1b[0m\n  \x1b[1mtest part of speech\x1b[0m\n    \
+            \x1b[0mtest definition\x1b[0m\n\n    \x1b[4mexample\x1b[0m\n      \
+            \x1b[3mtest example text\x1b[0m";
+
+        let fake_conf = FormatConf::default();
+
+        let word_str = format_word_data(&fake_conf, &fake_word);
+
+        assert_eq!(&word_str, fake_word_string, "did not format correctly");
+    }
 }
