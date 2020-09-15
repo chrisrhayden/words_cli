ae10f0fb5749fa9ba0df8d381442ea9dcea4d5fb
chrisrhayden <chrisrhayden@gmail.com>
2020-09-14 23:19:37 -0700

    add fake utils

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -2,6 +2,8 @@ use std::path::PathBuf;
 
 use tempfile::{tempdir, TempDir};
 
+use crate::dict_api::*;
+
 const MONSTER: &'static str = r#"{"word":"monster","phonetics":[{"text":"/ˈmɑnstər/","audio":"https://lex-audio.useremarkable.com/mp3/monster_us_1.mp3"}],"meanings":[{"partOfSpeech":"noun","definitions":[{"definition":"An imaginary creature that is typically large, ugly, and frightening.","example":"She was made into a horrid, ugly monster.","synonyms":["fabulous creature","mythical creature"]}]},{"partOfSpeech":"transitive verb","definitions":[{"definition":"Criticize or reprimand severely.","example":null,"synonyms":["criticize","censure","condemn","castigate","chastise","lambast","pillory","savage","find fault with","fulminate against","abuse"]}]}]}"#;
 
 #[derive(Default)]
@@ -29,4 +31,28 @@ impl Drop for TempSetup {
 
 pub fn fake_word_strings() -> Vec<(String, String)> {
     vec![("monster".to_string(), MONSTER.to_string())]
+}
+
+pub fn fake_word_data() -> WordData {
+    let phonetics = vec![Phonetic {
+        text: "test".to_string(),
+        audio: "test".to_string(),
+    }];
+
+    let definitions = vec![Definition {
+        definition: "test definition".to_string(),
+        example: Some("test example text".to_string()),
+        synonyms: None,
+    }];
+
+    let meanings = vec![Meaning {
+        partOfSpeech: "test part of speech".to_string(),
+        definitions,
+    }];
+
+    WordData {
+        word: "test".to_string(),
+        meanings,
+        phonetics,
+    }
 }
